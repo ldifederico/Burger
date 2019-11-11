36950488015c5e85ffc17cb500132beb93eadc49
lmd <difedericolinda@gmail.com>
2019-11-11 18:38:20 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "Big Mac", TRUE);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (2, "Whopper", FALSE);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (3, "Filet-O-Fish", TRUE);
