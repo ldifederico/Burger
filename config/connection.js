3bf3f60b51a28df09ffc1163584d343a0878f3d5
lmd <difedericolinda@gmail.com>
2019-11-12 15:49:58 -0500

    added functionality to nav toggle

--- config/connection.js
+++ config/connection.js
@@ -14,10 +14,8 @@ if(process.env.JAWSDB_URL) {
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
