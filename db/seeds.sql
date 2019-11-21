45fddefddbf4cc96a01e2b9fd21ceea01be80855
lmd <difedericolinda@gmail.com>
2019-11-21 13:13:28 -0500

    added default value

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Big Mac", TRUE);
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Whopper", FALSE);
+INSERT INTO burgers (burger_name)
+VALUES ("Whopper");
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Filet-O-Fish", TRUE);
